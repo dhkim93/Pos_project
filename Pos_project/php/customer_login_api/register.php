8fb505d3b6beaf3db9ecf6672deb56f7246748fd
dhkim93 <dhkim930@naver.com>
2016-09-08 16:55:08 +0900

    All of php Source code

---
+++ Pos_project/php/customer_login_api/register.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @author Ravi Tamada
+ * @link http://www.androidhive.info/2012/01/android-login-and-registration-with-php-mysql-and-sqlite/ Complete tutorial
+ */
+
+
+require_once 'include/DB_Functions.php';
+$db = new DB_Functions();
+
+// json response array
+$response = array("error" => FALSE);
+
+if (isset($_POST['name']) && isset($_POST['user_id']) && isset($_POST['password']) && isset($_POST['phone'])) {
+
+    // receiving the post params
+    $name = $_POST['name'];
+    $user_id = $_POST['user_id'];
+    $phone = $_POST['phone'];
+    $password = $_POST['password'];
+    
+
+    // check if user is already existed with the same email
+    if ($db->isUserExisted($user_id)) {
+        // user already existed
+        $response["error"] = TRUE;
+        $response["error_msg"] = "User already existed with " . $user_id;
+        echo json_encode($response);
+    } else {
+        // create a new user
+        $user = $db->storeUser($name, $user_id, $password, $phone);
+        if ($user) {
+            // user stored successfully
+            $response["error"] = FALSE;
+            $response["uid"] = $user["unique_id"];
+            $response["user"]["name"] = $user["name"];
+            $response["user"]["user_id"] = $user["user_id"];
+            $response["user"]["phone"] = $user["phone"];
+            $response["user"]["created_at"] = $user["created_at"];
+            $response["user"]["updated_at"] = $user["updated_at"];
+            echo json_encode($response);
+        } else {
+            // user failed to store
+            $response["error"] = TRUE;
+            $response["error_msg"] = "가입도중 알 수 없는 에러가 발생했습니다!";
+            echo json_encode($response);
+        }
+    }
+} else {
+    $response["error"] = TRUE;
+    $response["error_msg"] = iconv("EUC-KR","UTF-8", "이름, 휴대폰 번호 또는 패스워드가 빠졌습니다.");
+    echo json_encode($response);
+}
+?>
+
